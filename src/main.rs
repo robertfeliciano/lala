8b418c35802caed8517ee331649fc8096d175d45
robertfeliciano <robertfeliciano314@gmail.com>
2023-08-07 19:43:53 -0400

    why was that there

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ pub mod interp;
 fn main() -> Result<(), anyhow::Error> {
     let raw_file = std::fs::read_to_string("example.lala").expect("Cannot read lala file");
     let ast_root = lala::parser::parse(&raw_file).expect("unsuccessful parse");
-    // println!("{:?}", &ast_root);
+    println!("{:?}", &ast_root);
     interp::interp( &ast_root )?;
     Ok(())
 }
