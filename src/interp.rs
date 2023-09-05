fc9923990a4fda92557098dd9ce140595949e55d
robertfeliciano <robertfeliciano314@gmail.com>
2023-09-05 12:03:00 -0400

    interp backend pt1

--- src/interp.rs
+++ src/interp.rs
@@ -1,8 +1,56 @@
+use std::{collections::HashMap, ops::Deref, fmt::Display};
 use lala::parser::*;
 use anyhow::{anyhow, Context};
 
+pub enum LalaType{
+    Integer(i32),
+    Matrix(Vec<f64>),
+    Double(f64)
+}
+
+impl Display for LalaType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self{
+            LalaType::Integer(i) => write!(f, "{}", i.to_string())?,
+            LalaType::Double(d) => write!(f, "{}", d.to_string())?,
+            LalaType::Matrix(m) => todo!(),
+        };
+
+        Ok(())
+    }
+}
+
+fn eval_assignment(ident: &String, expr: &Box<AstNode>, env: &mut HashMap<String, LalaType>) -> Result<(), anyhow::Error>{
+    match expr.deref() {
+        AstNode::Integer(scalar) => 
+            match env.insert(ident.to_string(), LalaType::Integer(*scalar)) {
+                _ => Ok(()),
+            },
+        AstNode::DoublePrecisionFloat(scalar) => 
+            match env.insert(ident.to_string(), LalaType::Double(*scalar)) {
+                _ => Ok(()),
+            },
+        AstNode::Matrix(v) => todo!(),
+        AstNode::MonadicOp { verb, expr } => todo!(),
+        AstNode::DyadicOp { verb, lhs, rhs } => todo!(),
+        _ => Err(anyhow!("bruh"))
+    }
+}
+
 pub fn interp(ast: &Vec<Box<AstNode>>) -> Result<(), anyhow::Error>{
-    
+    let mut env: HashMap<String, LalaType> = HashMap::new();
+    for node in ast {
+        let _ = match node.deref() {
+            AstNode::Assignment { ident, expr } => eval_assignment(ident, expr, &mut env),
+            AstNode::MonadicOp { verb, expr } => todo!(),
+            AstNode::DyadicOp { verb, lhs, rhs } => todo!(),
+            AstNode::Ident(var) => {
+                println!("{}", env.get(var).unwrap());
+                Ok(())
+            },
+            bad_line => panic!("Invalid line: {:?}", bad_line),
+        };
+    }
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ pub mod parser;
 pub mod interp;
 
 fn main() -> Result<(), anyhow::Error> {
-    let raw_file = std::fs::read_to_string("example.lala").expect("Cannot read lala file");
+    let raw_file = std::fs::read_to_string("assn.lala").expect("Cannot read lala file");
     let ast_root = lala::parser::parse(&raw_file).expect("unsuccessful parse");
     println!("{:?}", &ast_root);
     interp::interp( &ast_root )?;
