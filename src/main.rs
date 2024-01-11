871915992dbcf970f32ff3d38f2a7985e80fbb43
robertfeliciano <robertfeliciano314@gmail.com>
2024-01-10 19:08:29 -0500

    bleh

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::path::Path;
 
+use anyhow::anyhow;
+
 pub mod interp;
 pub mod parser;
 pub mod repl;
@@ -8,16 +10,35 @@ gflags::define! {
     -c, --compile: &Path
 }
 
+fn get_extension(path: &str) -> Result<&str, anyhow::Error> {
+    let ext = Path::new(path).extension().and_then(|ext| ext.to_str()).unwrap();
+    if ext != "lala"{
+        Err(anyhow!("Not a lala file!"))
+    }
+    else {
+        Ok(ext)
+    }
+}
+
 fn main() -> Result<(), anyhow::Error> {
     let _patterns = gflags::parse();
     if COMPILE.is_present() {
         let path = COMPILE.flag.to_str().unwrap();
-        let raw_file = std::fs::read_to_string(path).expect("Cannot read lala file");
-        let ast_root = lala::parser::parse(&raw_file).expect("unsuccessful parse");
+        match get_extension(path) {
+            Ok(_) => {},
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                return Ok(());
+            },
+        }
+        
+        let raw_file = std::fs::read_to_string(path)?;
+
+
+        let ast_root = lala::parser::parse(&raw_file)?;
         interp::interp(&ast_root, None)?;
         Ok(())
     } else {
-        repl::repl();
-        Ok(())
+        repl::repl()
     }
 }
--- src/repl.rs
+++ src/repl.rs
@@ -8,16 +8,17 @@ use lala::types::*;
 
 use crate::interp;
 
-pub fn repl() {
+pub fn repl() -> Result<(), anyhow::Error> {
     let mut env: HashMap<String, LalaType> = HashMap::new();
+    println!("Lala Shell v0.0.1");
     loop {
-        print!("> ");
-        let _ = stdout().flush();
+        print!("\x1b[1mλ \x1b[0m");
+        stdout().flush()?;
         let mut input = String::new();
-        stdin().read_line(&mut input).unwrap();
+        stdin().read_line(&mut input)?;
+
         let line = input.trim();
-        let ast = lala::parser::parse(line).expect("unsuccessful parse");
-        // do question mark thing
-        let _ = interp(&ast, Some(&mut env));
+        let ast = lala::parser::parse(line)?;
+        interp(&ast, Some(&mut env))?;
     }
 }
